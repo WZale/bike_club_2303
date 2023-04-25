96b75b945fcb89f493a3546d08cadff482973d88
William Zale <williamzale@gmail.com>
2023-04-25 13:28:42 -0400

    Change default value of @rides hash, add log_ride method

--- lib/biker.rb
+++ lib/biker.rb
@@ -7,11 +7,24 @@ class Biker
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
-    @rides = Hash.new(0)
+    @rides = {}
     @acceptable_terrain = []
   end
 
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
+
+  def log_ride(ride, time)
+    while @acceptable_terrain.include?(ride.terrain) && ride.distance < @max_distance
+      if @rides[ride] == nil
+        @rides[ride] = []
+      end
+      @rides[ride] << time
+      require 'pry'; binding.pry
+    break
+    end
+  end
+
 end
+
