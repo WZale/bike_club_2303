55e9f288f3381b29374985e165438d4d9453f023
William Zale <williamzale@gmail.com>
2023-04-25 11:26:06 -0400

    Add test for initializing

---
+++ spec/biker_spec.rb
@@ -0,0 +1,19 @@
+require './lib/ride'
+require './lib/biker'
+
+RSpec.describe Ride do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  it "can initialize with readable attributes" do
+    expect(biker.name).to eq("Kenny")
+    expect(biker.max_distance).to eq(30)
+    expect(biker.rides).to eq({})
+    expect(biker.acceptable_terrain).to eq([])
+  end
+end
