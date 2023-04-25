b5767b5c3df60935c54af0469a37ea9c2dbfe3e2
William Zale <williamzale@gmail.com>
2023-04-25 13:29:50 -0400

    Add learn_terrain! assertions to test for log_ride method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -23,4 +23,22 @@ RSpec.describe Ride do
 
     expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
   end
+
+  it "has a log_ride method" do
+    @biker.learn_terrain!(:gravel)
+    @biker.learn_terrain!(:hills)
+    
+    @biker.log_ride(@ride1, 92.5)
+    @biker.log_ride(@ride1, 91.1)
+    @biker.log_ride(@ride2, 60.9)
+    @biker.log_ride(@ride2, 61.6)
+
+    expected =  {
+                @ride1 => [92.5, 91.1],
+                @ride2 => [60.9, 61.6]
+                }
+
+    expect(@biker.rides).to eq(expected)
+  end
+
 end
